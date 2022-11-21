466390ddfb0eb4a433df08c621e262a7405431de
Cdoran471 <cdoran@ltu.edu>
2022-11-21 16:14:10 -0500

    My stuff

---
+++ Assets/LaserStuff/HelperScripts/targetInfo.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class targetInfo
+{
+    //If a target is in range, this method will return true. You can provide a position and direction for the ray, as well as the range and the layer mask. Provides what was hit as HitInfo.
+    public static bool isTargetInRange(Vector3 rayPosition, Vector3 rayDirection, out RaycastHit HitInfo, float range, LayerMask mask)
+    {
+        return (Physics.Raycast(rayPosition, rayDirection, out HitInfo, range, mask));
+    }
+}
--- Assets/LaserStuff/SpaceshipShooting.cs
+++ Assets/LaserStuff/SpaceshipShooting.cs
@@ -1,10 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class SpaceshipShooting : MonoBehaviour
 {
+    [Header("=== Spaceship Settings ===")]
+    //[SerializeField] private SpaceshipMovement spaceship;
+
+
     [Header("=== Hardpoint Settings ===")]
     [SerializeField]
     private Transform[] hardpoint;
@@ -32,8 +37,66 @@ public class SpaceshipShooting : MonoBehaviour
 
     private bool firing;
 
+    private Camera cam;
+
+    private void Awake()
+    {
+        cam = Camera.main;
+    }
+
+    private void Update()
+    {
+        HandleLaserFiring();
+    }
+    private void HandleLaserFiring()
+    {
+        if (firing && !overHeated)
+        {
+            fireLaser();
+        }
+        else
+        {
+            foreach(var laser in lasers)
+            {
+                laser.gameObject.SetActive(false);
+            }
+
+            coolLaser();
+        }
+    }
+
+    void fireLaser()
+    {
+        RaycastHit HitInfo;
+        if (targetInfo.isTargetInRange(cam.transform.position, cam.transform.forward, out HitInfo, hardpointrange, shootableMask))
+        {
+            // Make hit particles here.
+            foreach (var laser in lasers)
+            {
+                Vector3 localHitPosition = laser.transform.InverseTransformPoint(HitInfo.point);
+                laser.gameObject.SetActive(true);
+                laser.SetPosition(1, localHitPosition);
+            }
+        }
+        else
+        {
+            foreach (var laser in lasers)
+            {
+                laser.gameObject.SetActive(true);
+                laser.SetPosition(1, new Vector3(0, 0, hardpointrange));
+            }
+        }
+    }
+
+
+    void coolLaser()
+    {
+        //Code to cool our laser.
+    }
+    #region Input
     public void onFire(InputAction.CallbackContext context)
     {
         firing = context.performed;
     }
+    #endregion
 }
