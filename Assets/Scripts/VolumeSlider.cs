6b059d716872c78381c83bda756dc8964f533276
Xiomie <mikegerxiong37@gmail.com>
2022-12-04 15:22:03 -0500

    Sound FX

---
+++ Assets/Scripts/VolumeSlider.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolumeSlider : MonoBehaviour
+{
+    [SerializeField] private SliderJoint2D _slider;
+    void Start()
+    {
+        
+    }
+
+
+}
